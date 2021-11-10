43ebefddc34e78ad5cdf0c246c6d2b48b70e7f24
Thomas Massmann <thomas.massmann@it-spir.it>
2021-11-10 18:33:13 +0100

    feat: :sparkles: add read more component

---
+++ src/components/section/ReadMore.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn :to="url" class="ml-2 my-2" nuxt outlined rounded small
+    >Read more
+    <span class="d-sr-only">about &quot;{{ title }}&quot;</span></v-btn
+  >
+</template>
+
+<script>
+export default {
+  name: 'PloneSectionReadMore',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
