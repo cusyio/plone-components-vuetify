988e286fba8fa6d3aad4f50873a75af8eaef3b82
Thomas Massmann <thomas.massmann@it-spir.it>
2021-11-10 11:11:14 +0100

    feat: :sparkles: add layout for documents

---
+++ src/components/layout/Document.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="wrapper" class="l-plone-document">
+    <PloneSectionHeading :title="content.title" :level="level" />
+    <PloneSectionDescription :description="content.description" />
+    <PloneSectionText :text="content.text" />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'PloneLayoutDocument',
+  props: {
+    content: {
+      type: Object,
+      required: true
+    },
+    level: {
+      type: [Number, String],
+      default: 1
+    },
+    wrapper: {
+      type: String,
+      default: 'div'
+    },
+  }
+};
+</script>
