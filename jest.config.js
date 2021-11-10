40daf4f00659ed9cd4ab564d44652301e83c8440
Thomas Massmann <thomas.massmann@it-spir.it>
2021-11-10 11:55:38 +0100

    chore: :wrench: prepare package config for tests and releases

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  testMatch: [
+    // Default
+    '**/test/**/*.js',
+    '**/__tests__/**/*.spec.js',
+    '**/__tests__/**/*.spec.ts',
+  ],
 };
